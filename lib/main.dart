f8b326b77602aad4f3087d78719d332243d0e0ad
AllahDitta37 <75024095+AllahDitta37@users.noreply.github.com>
2020-12-05 17:12:30 +0500

    error check after changing

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,55 @@
+
 import 'package:flutter/material.dart';
-import 'package:s17_bcs_004/homescreen.dart';
-import 'package:s17_bcs_004/splash.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
 
 
+void main() => runApp(SplashScreen());
 
-void main() {
-  runApp(
-    MyApp(),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
+class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MySplashScreen(),
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    );
+  }
+}
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => MyLevel())));
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/mypic.jpg', height: 150.0, width: 150,),
+          SizedBox(height: 20,),
+          Text(' SP17-BCS-004', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 60.0, color: Colors.black),),
+          SizedBox(height: 30,),
+          Text("Allah Ditta",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0, color: Colors.black),),
+
+          SizedBox( height: 30.0),
+
+          SpinKitRipple(color: Colors.blue),
+        ],
+      ),
     );
   }
 }
